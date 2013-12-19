0cf702bedafe62c0549942b42da8e9de96ba82ed
Eduardo San Martin Morote <posva13@gmail.com>
2013-12-19 11:18:28 +0100

    do.sh example

--- do.sh
+++ do.sh
@@ -4,9 +4,12 @@
 # I usually have this kind of script in my project folder so that I don't have to manually write the configure options everytime I add new dependencies to the project
 # This one is what I use in my project InputManager:
 # https://github.com/posva/InputManager
+# This script works on Windows. Installing cygwin is the easiest way to get this working
 
 if [ `uname` = "Darwin" ]; then
-  ./configure.sh -aD -s src -o obj -b bin -c "xcrun clang++" -O "-Wall -Wextra -O2 -std=c++11 -stdlib=libc++" -Isrc -L/usr/local/lib -e cpp -E main -M Makefile -l "-framework sfml-system -framework sfml-window -framework sfml-graphics" -N "-std=c++11 -stdlib=libc++"
+  ./configure.sh -aD -s src -o obj -b bin -c "xcrun clang++" -O "-Wall -Wextra -O2 -std=c++11 -stdlib=libc++" -Isrc -L/usr/local/lib -e cpp -E main -M Makefile -l "-framework sfml-system -framework sfml-window -framework sfml-graphics" -N "-std=c++11 -stdlib=libc++" $@
+elif [ `uname` = "Linux" ]; then
+  ./configure.sh -aD -s src -o obj -b bin -c "clang++" -O "-Wall -Wextra -O2 -std=c++11 -stdlib=libc++" -Isrc -L/usr/local/lib -e cpp -E main -M Makefile -l "-lsfml-system -lsfml-window -lsfml-graphics" -N "-std=c++11 -stdlib=libc++" $@
 else
-  ./configure.sh -aD -s src -o obj -b bin -c "clang++" -O "-Wall -Wextra -O2 -std=c++11 -stdlib=libc++" -Isrc -L/usr/local/lib -e cpp -E main -M Makefile -l "-lsfml-system -lsfml-window -lsfml-graphics" -N "-std=c++11 -stdlib=libc++"
+  ./configure.sh -aD -s src -o obj -b bin -c "mingw32-g++" -O "-Wall -Wextra -O2 -std=c++11 -stdlib=libc++" -Isrc -e cpp -E main -M Makefile -l "-lsfml-system -lsfml-window -lsfml-graphics" -N "-std=c++11 -stdlib=libc++" $@
 fi
